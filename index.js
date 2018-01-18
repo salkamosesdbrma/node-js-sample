c803bc3761cfd690cce7b5c9f85db819aef4a9f8
salkamoses <salkad@flock.com>
2018-01-18 18:01:00 +0530

    google trends data test

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 var express = require('express')
+var trends = require('google-trends')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+	trends.load(['in'], function (err, result) {
+		var res= "";
+		var test = JSON.parse(JSON.stringify(result));
+		var objectKeysArray = Object.keys(test);
+		objectKeysArray.forEach(function(objKey) {
+			var objValue = test[objKey];
+			objValue.forEach(function(trenddata) {
+				res += trenddata.title + "<br/>";
+			})
+		})
+		response.send(res)
+	})
 })
 
 app.listen(app.get('port'), function() {
