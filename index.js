6d5fbc9c6f4adced9c8c996e42bf2878ec07c3d3
salkamoses <salkad@flock.com>
2018-01-19 00:44:46 +0530

    ejs view trend parsing

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 var express = require('express')
+
 var trends = require('google-trends')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+app.set('view engine', 'ejs');
+
+app.get('/test', function(request, response) {
+ 
+	trends.load(['kr'], function (err, result) {
+	  console.log(err, JSON.stringify(result))
+	  response.render('pages/test',{trends: JSON.parse(JSON.stringify(result))})
+	})
+})
+
 app.get('/', function(request, response) {
-	trends.load(['in'], function (err, result) {
-		var res= "";
-		var test = JSON.parse(JSON.stringify(result));
-		var objectKeysArray = Object.keys(test);
-		objectKeysArray.forEach(function(objKey) {
-			var objValue = test[objKey];
-			objValue.forEach(function(trenddata) {
-				res += trenddata.title + "<br/>";
-			})
-		})
-		response.send(res)
+	trends.load(['kr'], function (err, result) {
+		response.send(JSON.parse(JSON.stringify(result)));
 	})
 })
 
